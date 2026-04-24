3b282b97fc68009e75d4cc91853875b09b4d468d
Luan Tenorio <lluantenorio7@gmail.com>
2026-04-24 20:43:06 -0300

    Add layout

--- src/main/java/com/github/luantenorio/projetocopatp1/HelloApplication.java
+++ src/main/java/com/github/luantenorio/projetocopatp1/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/github/luantenorio/projetocopatp1/panel/PanelController.java
+++ src/main/java/com/github/luantenorio/projetocopatp1/panel/PanelController.java
@@ -1,4 +1,30 @@
 package com.github.luantenorio.projetocopatp1.panel;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+
 public class PanelController {
+
+    @FXML
+    private VBox panel;
+
+    @FXML
+    public void initialize() {
+        this.forceRoundedEdges();
+    }
+
+    private void forceRoundedEdges(){
+        Rectangle clip = new Rectangle();
+
+        clip.setX(-20);
+        clip.setY(0);
+        clip.widthProperty().bind(panel.widthProperty().add(20));
+        clip.heightProperty().bind(panel.heightProperty());
+        clip.setArcWidth(40);
+        clip.setArcHeight(40);
+
+        panel.setClip(clip);
+    }
+
 }
