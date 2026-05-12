81494a46d88acf6c2046da8cce825daf2ec45df2
Mediador09 <fer.amdias@gmail.com>
2026-05-12 11:46:50 -0300

    refactor(match)

--- src/main/java/com/github/luantenorio/projetocopatp1/match/MatchEntity.java
+++ src/main/java/com/github/luantenorio/projetocopatp1/match/MatchEntity.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class MatchEntity implements Serializable {
@@ -16,8 +17,7 @@ public class MatchEntity implements Serializable {
     private String stadiumId;
     private String stage;
     private MatchStatus status;
-    private String score;
-    private final HashSet<MatchEvent> history = new HashSet<>();
+    private final Result result = new Result("0-0");
 
     public MatchEntity(String team1Id, String team2Id, ZonedDateTime date, String stadiumId, String stage) {
         this.id = UUID.randomUUID().toString();
@@ -28,24 +28,47 @@ public class MatchEntity implements Serializable {
         this.stage = stage;
     }
 
-    public MatchEntity(ZonedDateTime date, String stadiumId, String stage) {
+    public MatchEntity(String team1Id, String team2Id, ZonedDateTime date, String stadiumId, String stage, MatchStatus status) {
         this.id = this.id = UUID.randomUUID().toString();
+        this.team1Id = team1Id;
+        this.team2Id = team2Id;
         this.date = date;
         this.stadiumId = stadiumId;
         this.stage = stage;
+        this.status = status;
     }
 
-    public MatchEntity(String id, String team1Id, String team2Id, ZonedDateTime date, String stadiumId, String stage) {
+    public MatchEntity(String id, String team1Id, String team2Id, ZonedDateTime date, String stadiumId, String stage, MatchStatus status) {
         this.id = id;
         this.team1Id = team1Id;
         this.team2Id = team2Id;
         this.date = date;
         this.stadiumId = stadiumId;
         this.stage = stage;
+        this.status = status;
+    }
+
+    public void setScore(String score){
+        if (status != MatchStatus.SCHEDULED){
+            this.result.setScore(score);
+        }
+    }
+
+    public String getScore(String score){
+        return result.getScore();
+    }
+
+    public Set<MatchEvent> getHistory(){
+        if (this.status != MatchStatus.SCHEDULED) return result.getHistory();
+        return null;
     }
 
     public String getId() {
         return id;
+    }
+
+    public void addEvent(String minute, EventType type, String description){
+        if (this.status != MatchStatus.SCHEDULED) result.addEvent(minute, type, description);
     }
 
     public String getTeam1Id() {
@@ -68,12 +91,13 @@ public class MatchEntity implements Serializable {
         return stage;
     }
 
-    public void setStatus(MatchStatus status) {
-        this.status = status;
-    }
-
     public MatchStatus getStatus() {
         return status;
+    }
+
+    public void setStatus(MatchStatus status) {
+        this.status = status;
+        if (status == MatchStatus.SCHEDULED) result.setScore("0-0");
     }
 
     public void setTeam1(@NotNull TeamEntity team1) {
@@ -84,26 +108,35 @@ public class MatchEntity implements Serializable {
         this.team2Id = team2.getId();
     }
 
-    public String getScore() {
-        return score;
-    }
+    private class Result {
+        private String score;
+        private final HashSet<MatchEvent> history = new HashSet<>();
 
-    public void setScore(String score) {
-        this.score = score;
-    }
+        public Result(String score) {
+            this.score = score;
+        }
 
-    public HashSet<MatchEvent> getHistory() {
-        return history;
-    }
+        public String getScore() {
+            return score;
+        }
 
-    public void addEvent(String minute, EventType type, String description){
-        history.add(
-                new MatchEvent(
-                    UUID.randomUUID().toString(),
-                    minute,
-                    type,
-                    description
-                )
-        );
+        public void setScore(String score) {
+            this.score = score;
+        }
+
+        public Set<MatchEvent> getHistory() {
+            return history;
+        }
+
+        public void addEvent(String minute, EventType type, String description){
+            history.add(
+                    new MatchEvent(
+                            UUID.randomUUID().toString(),
+                            minute,
+                            type,
+                            description
+                    )
+            );
+        }
     }
 }
