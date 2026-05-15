107abe996388dd3c27f5be0686654ab7b117939f
Luan Tenorio <lluantenorio7@gmail.com>
2026-05-15 15:20:35 -0300

    Save stadium

--- src/main/java/com/github/luantenorio/projetocopatp1/stadium/CreateStadiumController.java
+++ src/main/java/com/github/luantenorio/projetocopatp1/stadium/CreateStadiumController.java
@@ -1,5 +1,7 @@
 package com.github.luantenorio.projetocopatp1.stadium;
 
+import com.github.luantenorio.projetocopatp1.util.Router;
+import com.github.luantenorio.projetocopatp1.util.ViewName;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
@@ -13,6 +15,8 @@ public class CreateStadiumController {
     @FXML
     private TextField txtCapacity;
 
+    private final StadiumService stadiumService = new StadiumService();
+
     @FXML
     public void initialize(){
         this.formatCapacityField();
@@ -22,6 +26,11 @@ public class CreateStadiumController {
         String name = txtName.getText();
         String location = txtLocation.getText();
         int capacity = Integer.parseInt(txtCapacity.getText());
+
+        StadiumEntity stadium = new StadiumEntity(name, location, capacity);
+
+        this.stadiumService.createStadium(stadium);
+        Router.NavigateTo(ViewName.STADIUM);
     }
 
     private void formatCapacityField(){
