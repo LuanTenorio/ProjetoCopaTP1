c98a72e7cf0af41f649756311ca1ce2674347fc8
Luan Tenorio <lluantenorio7@gmail.com>
2026-05-15 16:04:22 -0300

    Add title

--- src/main/java/com/github/luantenorio/projetocopatp1/util/ViewName.java
+++ src/main/java/com/github/luantenorio/projetocopatp1/util/ViewName.java
@@ -1,9 +1,9 @@
 package com.github.luantenorio.projetocopatp1.util;
 
 public enum ViewName {
-    STADIUM("stadium.fxml"),
-    CREATE_STADIUM("create_stadium.fxml"),
-    PLAYER("player.fxml");
+    STADIUM("stadium.fxml|Estádios"),
+    CREATE_STADIUM("create_stadium.fxml|Criar estádio"),
+    PLAYER("player.fxml|Jogadores");
 
     private final String fileFxmlName;
 
