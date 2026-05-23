1e2e91e1c0308a354909feb4ccce303a43010831
João Victor <joaovictorsilvafreitas@hotmail.com>
2026-05-23 20:42:11 -0300

    feat: cadastro de seleções

--- src/main/java/com/github/luantenorio/projetocopatp1/team/CreateTeamController.java
+++ src/main/java/com/github/luantenorio/projetocopatp1/team/CreateTeamController.java
@@ -1,18 +1,28 @@
 package com.github.luantenorio.projetocopatp1.team;
 
 import com.github.luantenorio.projetocopatp1.player.PlayerEntity;
+import com.github.luantenorio.projetocopatp1.player.PlayerService;
 import com.github.luantenorio.projetocopatp1.player.PlayerStatus;
+import com.github.luantenorio.projetocopatp1.stadium.StadiumEntity;
+import com.github.luantenorio.projetocopatp1.util.Router;
+import com.github.luantenorio.projetocopatp1.util.ViewName;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CreateTeamController {
 
-    private final ObservableList<PlayerEntity> availablePlayers = FXCollections.observableArrayList();
-    private final ObservableList<PlayerEntity> selectedPlayers = FXCollections.observableArrayList();
+    private final TeamService teamService = new TeamService();
+    private final PlayerService playerService = new PlayerService();
+
+    private ObservableList<PlayerEntity> availablePlayers;
+    private ObservableList<PlayerEntity> selectedPlayers;
 
     @FXML
     private TextField txtName;
@@ -30,30 +40,33 @@ public class CreateTeamController {
     private ListView<PlayerEntity> selectedPlayersListView;
 
     @FXML
+    private Label selectedPlayersCountLabel;
+
+    @FXML
     private Button addPlayerButton;
 
     @FXML
     private Button removePlayerButton;
 
     @FXML
+    private Button buttonOperate;
+
     public void initialize() {
-        this.availablePlayers.addAll(
-                new PlayerEntity("Neymar", "ATA", 1, 0, PlayerStatus.ACTIVE),
-                new PlayerEntity("Vini Jr", "ATA", 2, 0, PlayerStatus.ACTIVE),
-                new PlayerEntity("Casemiro", "DEF", 3, 0, PlayerStatus.INJURED),
-                new PlayerEntity("Lucas Paquetá", "ATA", 4, 0, PlayerStatus.INJURED),
-                new PlayerEntity("Martinelli", "MEI", 5, 0, PlayerStatus.ACTIVE)
-        );
+        List<PlayerEntity> players = playerService.findAll();
+
+        this.availablePlayers = FXCollections.observableArrayList(players);
+        this.selectedPlayers = FXCollections.observableArrayList();
 
         this.availablePlayersListView.setItems(this.availablePlayers);
         this.selectedPlayersListView.setItems(this.selectedPlayers);
+        this.selectedPlayersCountLabel.textProperty()
+                .bind(Bindings.size(this.selectedPlayers).asString("Selecionados: %d"));
 
         this.availablePlayersListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         this.selectedPlayersListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
-    @FXML
-    private void addSelectedPlayers() {
+    public void addSelectedPlayers() {
         List<PlayerEntity> selectedPlayers = new ArrayList<>(this.availablePlayersListView.getSelectionModel().getSelectedItems());
 
         if (!selectedPlayers.isEmpty()) {
@@ -63,8 +76,7 @@ public class CreateTeamController {
         }
     }
 
-    @FXML
-    private void removeSelectedPlayers() {
+    public void removeSelectedPlayers() {
         List<PlayerEntity> selectedPlayers = new ArrayList<>(this.selectedPlayersListView.getSelectionModel().getSelectedItems());
 
         if (!selectedPlayers.isEmpty()) {
@@ -72,6 +84,60 @@ public class CreateTeamController {
             this.availablePlayers.addAll(selectedPlayers);
             this.selectedPlayersListView.getSelectionModel().clearSelection();
         }
+    }
+
+    public void operate() {
+        if (!isInputValid()) {
+            return;
+        }
+        try {
+            registerTeam();
+            backToPreviousView();
+        } catch (TeamException e) {
+            showInvalidMessage(e.getMessage());
+        }
+    }
+
+    private void registerTeam() {
+        teamService.createTeam(getCurrentEntity());
+    }
+
+    private void backToPreviousView() {
+        Router.navigateTo(ViewName.TEAM);
+    }
+
+    private boolean isInputValid() {
+        String error = "";
+
+        if (txtName.getText() == null || txtName.getText().trim().isEmpty())
+            error += "O campo 'Nome' é obrigatório.\n";
+
+        if (groupComboBox.getValue() == null || groupComboBox.getValue().trim().isEmpty())
+            error += "O campo 'Grupo' é obrigatório.\n";
+
+        if (txtCoach.getText() == null || txtCoach.getText().trim().isEmpty())
+            error += "O campo 'Técnico' é obrigatório.\n";
+
+        if (error.isEmpty())
+            return true;
+
+        this.showInvalidMessage(error);
+        return false;
+    }
+
+    private void showInvalidMessage(String error){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Campos inválidos");
+        alert.setContentText(error);
+        alert.showAndWait();
+    }
+
+    private TeamEntity getCurrentEntity(){
+        String name = txtName.getText();
+        String group = groupComboBox.getValue();
+        String coach = txtCoach.getText();
+
+        return new TeamEntity(name, group, coach, this.selectedPlayers);
     }
 
 }
