b50d325d4869675b1ccf1be9d3fcbc6f1363ab9a
João Victor <joaovictorsilvafreitas@hotmail.com>
2026-05-27 22:12:06 -0300

    add: label on playerStatus

--- src/main/java/com/github/luantenorio/projetocopatp1/player/PlayerStatus.java
+++ src/main/java/com/github/luantenorio/projetocopatp1/player/PlayerStatus.java
@@ -1,7 +1,18 @@
 package com.github.luantenorio.projetocopatp1.player;
 
 public enum PlayerStatus {
-    ACTIVE,
-    INJURED,
-    SUSPENDED
+    ACTIVE("Ativo"),
+    INJURED("Lesionado"),
+    SUSPENDED("Suspenso");
+
+    private final String label;
+
+    PlayerStatus(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
 }
